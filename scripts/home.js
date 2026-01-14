2951704a7f743803a5900da921705c4a91372f61
Marcello <marcellomino@gmail.com>
2026-01-14 17:57:17 -0500

    added js for rock paper scissor game and made results pop up on screen as well as used event listeners

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,7 @@
+
+//this is the function for heads or tails
 const flipCoin = function (){
+
     let result = '';
     let randomNum = Math.random();
 
@@ -18,3 +21,61 @@ function resetFlip(){
 
 document.querySelector('.flip-button').addEventListener('click', flipCoin);
 document.querySelector('.reset-flip').addEventListener('click', resetFlip);
+
+//this is the functions for rock paper scissor game 
+
+function computerMove (){
+    let computerMove = '';
+    let randomNum = Math.random();
+
+    if(randomNum <= 1/3){
+        computerMove = 'rock';
+    } else if(randomNum > 1/3 && randomNum <= 2/3){
+        computerMove = 'paper';
+    } else if (randomNum > 2/3 && randomNum <= 1){
+        computerMove = 'scissor'
+    }
+
+    return computerMove
+}
+
+
+function playGame(playerMove){
+    let result = '';
+    const move = computerMove();
+
+    if (playerMove === 'rock'){
+        if(move === 'rock'){
+            result = 'Tie';
+        } else if(move === 'paper'){
+            result = 'You Lose';
+        } else if (move === 'scissor'){
+            result = 'You Win';
+        }
+    } else if(playerMove === 'paper'){
+        if (move === 'rock'){
+            result = 'You Win';
+        } else if (move === 'paper'){
+            result = 'Tie';
+        } else if (move === 'scissor'){
+            result = 'You Lose';
+        }
+    } else if (playerMove === 'scissor'){
+        if(move === 'rock'){
+            result = 'You Lose';
+        } else if (move === 'paper'){
+            result = 'You Win';
+        } else if (move === 'scissor'){
+            result = 'Tie';
+        }
+    }
+
+    document.querySelector('.rock-paper-scissor-result').textContent = `You chose ${playerMove}. Computer chose ${move}. ${result}`
+}
+
+document.querySelector('.rock-button').addEventListener('click', () => playGame('rock'));
+document.querySelector('.paper-button').addEventListener('click', () => playGame('paper'));
+document.querySelector('.scissor-button').addEventListener('click', () => playGame('scissor'));
+
+
+
