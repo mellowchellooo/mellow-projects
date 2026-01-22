8dcc01e1aa8fb80912762d9e06568d59f3fd21a9
Marcello <marcellomino@gmail.com>
2026-01-22 15:49:36 -0500

    added coin flip to its own page for clarity, will remove from home page and replace with a link

---
+++ scripts/coin-flip.js
@@ -0,0 +1,21 @@
+const flipCoin = function (){
+
+    let result = '';
+    let randomNum = Math.random();
+
+    if (randomNum <= 1/2){
+        result = 'Heads';
+    } else {
+        result = 'Tails';
+    }
+
+    document.querySelector('.flip-result').textContent = result;
+}
+
+function resetFlip(){
+    let result = '';
+    document.querySelector('.flip-result').textContent = result;
+}
+
+document.querySelector('.flip-button').addEventListener('click', flipCoin);
+document.querySelector('.reset-flip').addEventListener('click', resetFlip);
